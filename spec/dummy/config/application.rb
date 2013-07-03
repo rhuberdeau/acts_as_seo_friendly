ac11843d5647a3b2027632516df7c3dbc937ce74
rhuberdeau <rhuberdeau@gmail.com>
2013-07-02 21:00:51 -0400

    preparing dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,7 +8,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "foobar"
+require "acts_as_seo_friendly"
 
 module Dummy
   class Application < Rails::Application
